646ba9439766de3a40c35839a0dcda93d85fea22
Corsi1 <angelomillancorsini@gmail.com>
2026-03-29 17:24:13 -0400

    Ping pong

--- src/main.c
+++ src/main.c
@@ -1,1 +1,17 @@
 #include <stdio.h>
+#include <windows.h>
+#define V 50
+#define H 50
+
+int main(){
+
+    int pelX, pelY, INIJU, FINALJU, iniIA, finIA;
+    int modX, modY, modIA;
+    char tablero[V][H];
+
+    pelX = 25;
+    pelY = 25;
+    
+
+}
+
